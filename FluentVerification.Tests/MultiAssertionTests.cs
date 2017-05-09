5e3e98cc973ab2d4fa16298cfa1ca53b5414cceb
Lucas Obendorf <Lucas.Obendorf@gmail.com>
2017-05-08 21:01:52 -0600

    Added AtMost, Between, All, and None actions to MultiAssert.

--- FluentVerification.Tests/MultiAssertionTests.cs
+++ FluentVerification.Tests/MultiAssertionTests.cs
@@ -124,5 +124,200 @@ namespace FluentVerification.Tests
                 });
             }
         }
+
+        [Test]
+        public void AtLeast_Only_Counts_Successful_Assertions()
+        {
+            var list = new List<int>
+            {
+                1,
+                2,
+                3,
+                1
+            };
+
+            Assert.DoesNotThrow(() =>
+            {
+                Check.That(list)
+                    .With(x => x.AtLeast(2,
+                        once => once.EqualTo(1)));
+            });
+
+        }
+
+        [TestCase(1, 1, true)
+        ,TestCase(2, 1, false)
+        ,TestCase(0, 1, true)]
+        public void AtMost_Handles_Expected_Range(int items, int atMost, bool shouldSucceed)
+        {
+            var list = new List<string>();
+
+            for (var i = 0; i < items; i++)
+            {
+                list.Add(Guid.NewGuid().ToString());
+            }
+
+            if (shouldSucceed)
+            {
+                Assert.DoesNotThrow(() =>
+                {
+                    Check.That(list)
+                        .With(x => x.AtMost((uint)atMost,
+                            item => item.NotNull()));
+                });
+            }
+            else
+            {
+                Assert.Throws<AssertionException>(() =>
+                {
+                    Check.That(list)
+                        .With(x => x.AtMost((uint)atMost,
+                            item => item.NotNull()));
+                });
+            }
+        }
+
+        [Test]
+        public void AtMost_Only_Counts_Successful_Assertions()
+        {
+            var list = new List<int>
+            {
+                1,
+                2,
+                3,
+                1
+            };
+
+            Assert.DoesNotThrow(() =>
+            {
+                Check.That(list)
+                    .With(x => x.AtMost(2,
+                        once => once.EqualTo(1)));
+            });
+
+        }
+
+        [TestCase(1, 1, 2, true)
+        ,TestCase(2, 1, 2, true)
+        ,TestCase(0, 1, 2, false)
+        ,TestCase(2, 1, 3, true)
+        ,TestCase(3, 1, 2, false)]
+        public void Between_Handles_Expected_Range(int items, int min, int max, bool shouldSucceed)
+        {
+            var list = new List<string>();
+
+            for (var i = 0; i < items; i++)
+            {
+                list.Add(Guid.NewGuid().ToString());
+            }
+
+            if (shouldSucceed)
+            {
+                Assert.DoesNotThrow(() =>
+                {
+                    Check.That(list)
+                        .With(x => x.Between((uint)min, (uint)max,
+                            item => item.NotNull()));
+                });
+            }
+            else
+            {
+                Assert.Throws<AssertionException>(() =>
+                {
+                    Check.That(list)
+                        .With(x => x.Between((uint)min, (uint)max,
+                            item => item.NotNull()));
+                });
+            }
+        }
+
+        [Test]
+        public void Between_Only_Counts_Successful_Assertions()
+        {
+            var list = new List<int>
+            {
+                1,
+                2,
+                3,
+                1
+            };
+
+            Assert.DoesNotThrow(() =>
+            {
+                Check.That(list)
+                    .With(x => x.Between(1, 2,
+                        once => once.EqualTo(1)));
+
+                Check.That(list)
+                    .With(x => x.Between(2, 3,
+                        item => item.EqualTo(1)));
+            });
+        }
+
+        [Test]
+        public void All_Passes_On_Condition_Met()
+        {
+            var list = new List<int>
+            {
+                1,
+                1
+            };
+
+            Assert.DoesNotThrow(() =>
+            {
+                Check.That(list)
+                     .With(x => x.All(item => item.EqualTo(1)));
+            });
+        }
+
+        [Test]
+        public void All_Errors_On_Condition_Failed()
+        {
+            var list = new List<int>
+            {
+                1,
+                1,
+                2
+            };
+
+            Assert.Throws<AssertionException>(() =>
+            {
+                Check.That(list)
+                    .With(x => x.All(item => item.EqualTo(1)));
+            });
+        }
+
+        [Test]
+        public void None_Passes_On_Condition_Met()
+        {
+            var list = new List<int>
+            {
+                2,
+                2
+            };
+
+            Assert.DoesNotThrow(() =>
+            {
+                Check.That(list)
+                    .With(x => x.None(item => item.EqualTo(1)));
+            });
+        }
+
+        [Test]
+        public void None_Errors_On_Condition_Failed()
+        {
+            var list = new List<int>
+            {
+                2,
+                2,
+                1
+            };
+
+            Assert.Throws<AssertionException>(() =>
+            {
+                Check.That(list)
+                    .With(x => x.None(item => item.EqualTo(1)));
+            });
+        }
     }
 }
--- FluentVerification/MultiAssertionExtenders.cs
+++ FluentVerification/MultiAssertionExtenders.cs
@@ -44,5 +44,61 @@ namespace FluentVerification
         {
             return assertion.Within(times, null, handler);
         }
+
+        public static MultiAssertion<T> AtMost<T>(this MultiAssertion<T> assertion, uint times, Action<IAssertion<T>> handler)
+        {
+            return assertion.Within(null, times, handler);
+        }
+
+        public static MultiAssertion<T> Between<T>(this MultiAssertion<T> assertion, uint min, uint max, Action<IAssertion<T>> handler)
+        {
+            return assertion.Within(min, max, handler);
+        }
+
+        public static MultiAssertion<T> All<T>(this MultiAssertion<T> assertion, Action<IAssertion<T>> handler)
+        {
+            assertion.Handle(items =>
+            {
+                foreach (var item in items)
+                {
+                    var itemAssertion = new Assertion<T>(item);
+
+                    try
+                    {
+                        handler(itemAssertion);
+                    }
+                    catch
+                    {
+                        throw new AssertionException("Not all items matched condition");
+                    }
+                }
+            });
+
+            return assertion;
+        }
+
+        public static MultiAssertion<T> None<T>(this MultiAssertion<T> assertion, Action<IAssertion<T>> handler)
+        {
+            assertion.Handle(items =>
+            {
+                foreach (var item in items)
+                {
+                    var itemAssertion = new Assertion<T>(item);
+
+                    try
+                    {
+                        handler(itemAssertion);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    throw new AssertionException("An item met a condition it was not supposed to.");
+                }
+            });
+
+            return assertion;
+        }
     }
 }
